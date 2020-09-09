be352836a43d3596bfdc1caa0f51eca24d10f433
Matheus Cândido <matheus9bh@hotmail.com>
2020-09-09 15:41:52 -0300

    add-sidebar-navigation

--- src/App.js
+++ src/App.js
@@ -5,21 +5,32 @@ import { useSelector } from 'react-redux';
 import HomeScreen from './pages/HomeScreen';
 import Tela2Screen from './pages/Tela2Screen';
 
+import MenuItem from './components/MenuItem';
+
+import { Container, Menu, Body } from './AppStyled';
+
 export default () => {
     const name = useSelector(state => state.user.name);
 
     return (
         <BrowserRouter>
-            <h1>Oi, {name}</h1>
-
-            <Switch>
-                <Route exact path="/">
-                    <HomeScreen />
-                </Route>
-                <Route path="/tela2/:nome">
-                    <Tela2Screen />
-                </Route>
-            </Switch>
+            <Container>
+                <Menu>
+                    <MenuItem icon="/assets/store.png" link="/" />
+                    <MenuItem icon="/assets/order.png" link="/orders" />
+                    <MenuItem icon="/assets/profile.png" link="/profile" />
+                </Menu>
+                <Body>
+                    <Switch>
+                        <Route exact path="/">
+                            <HomeScreen />
+                        </Route>
+                        <Route path="/tela2/:nome">
+                            <Tela2Screen />
+                        </Route>
+                    </Switch>
+                </Body>
+            </Container>
         </BrowserRouter>
     );
 }
